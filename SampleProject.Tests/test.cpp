db26fb9ed32df9677f4ade638f6a14bf3d9da667
alina.gheorghe <alina.gheorghe@mta.ro>
2024-12-14 16:54:14 +0200

    add tests + opencpp settings

--- SampleProject.Tests/test.cpp
+++ SampleProject.Tests/test.cpp
@@ -1,6 +1,27 @@
 #include "pch.h"
+#include "../SampleProject.Sources/MathFunctions.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(MathOperationsTest, Add)
+{
+	EXPECT_EQ(add(2, 2), 4);
+	EXPECT_EQ(add(-1, 1), 0);
+	EXPECT_EQ(add(0, 0), 0);
+}
+
+TEST(MathOperationsTest, Subtract)
+{
+	EXPECT_EQ(substract(5, 3), 2);
+	EXPECT_EQ(substract(3, 5), -2);
+}
+
+TEST(MathOperationsTest, Divide)
+{
+	EXPECT_EQ(divide(10, 2), 5);
+	//EXPECT_EQ(divide(10, 2), 7);
+	EXPECT_THROW(divide(1, 0), std::invalid_argument);
+}
+
+TEST(MathOperationsTest, Multiply)
+{
+	EXPECT_EQ(multiply(10, 2), 20);
 }
